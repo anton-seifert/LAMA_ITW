b21ee3df6b824b9b8dd8ec38b6c4828d41391669
Arne Segatz <a.seg@gmx.de>
2026-01-17 18:57:42 +0100

    func

--- train/test_train.py
+++ train/test_train.py
@@ -4,6 +4,7 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.env_checker import check_env
 from stable_baselines3.common.vec_env import VecNormalize, SubprocVecEnv
 from stable_baselines3.common.env_util import make_vec_env
+from typing import Optional
 import os
 import sys
 import time
@@ -17,7 +18,8 @@ sys.path.append(parent_dir)
 
 from envs.test_env import RobotWorldEnv
 
-def train():
+def train(env, config: Optional[dict] = None):
+    #TODO: variablem mit config.get() holen
     folder_name = "assets"
     file_name = "test_robot.xml"
     modelpath = os.path.join(folder_name, file_name)
@@ -25,17 +27,22 @@ def train():
     save_dir = "models/ppo_training/"
     os.makedirs(save_dir, exist_ok=True)
 
-    env = RobotWorldEnv(model_path=modelpath)
+    env_control = RobotWorldEnv(model_path=modelpath)
     #Checks if Costum env corresponds GymAPI  
-    check_env(env)
-    env.close()
+    check_env(env_control)
+    env_control.close()
 
-    vec_env = make_vec_env(RobotWorldEnv,n_envs=6, env_kwargs={"model_path":modelpath},vec_env_cls=SubprocVecEnv)
+    vec_env = make_vec_env(env,n_envs=6, env_kwargs={"model_path":modelpath},vec_env_cls=SubprocVecEnv)
     vec_env = VecNormalize(vec_env, norm_obs=True, norm_reward=True)
 
-    model = PPO("MultiInputPolicy" ,vec_env, device="cpu" ,tensorboard_log ="./tensorboard/ppo_test_robot_tensorboard/", learning_rate=0.0003)
+    model = PPO("MultiInputPolicy" ,vec_env, device="cpu" ,tensorboard_log ="./tensorboard/ppo_test_robot_tensorboard/", 
+                policy_kwargs = dict(net_arch=[256, 256]),
+                n_steps=4096,
+                batch_size=256,
+                ent_coef=0.001,
+                learning_rate=lambda progress: progress * 0.0003)
 
-    model.learn(total_timesteps=1_000_000,progress_bar=True)
+    model.learn(total_timesteps=1_00_000,progress_bar=True)
 
     timestamp = time.strftime("%Y%m%d-%H%M%S")
     model_save_path = f"{save_dir}/ppo_test_robot_{timestamp}"
@@ -47,5 +54,6 @@ def train():
     vec_env.close()
 
 if __name__ == '__main__':
-   
-    train()
+
+    
+    train(RobotWorldEnv)
