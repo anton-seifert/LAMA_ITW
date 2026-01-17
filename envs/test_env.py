bfea09cf46669943b837a621903365200b366287
Anton Seifert <anton.seifert@web.de>
2026-01-17 04:21:45 +0100

    done

--- envs/test_env.py
+++ envs/test_env.py
@@ -1,18 +1,22 @@
 import gymnasium as gym
 import numpy as np
 import mujoco
+from typing import Optional
 
-
-class RobotWorldEnv(gym.MujocoEnv):
+class RobotWorldEnv(gym.Env):
     
     def __init__(self, model_path: str):
     
         #load model from Path
-        self.robot_model = mujoco.MjModel.from_xml_path(model_path)
-        self.data = mujoco.MjData(self.robot_model)
+        self.model = mujoco.MjModel.from_xml_path(model_path)
+        self.data = mujoco.MjData(self.model)
 
         self.target_pos = np.array(1,1,1) # just for initialition
+        self.steps_passed = 0
+        self.goal_distance = 0.1
         self.info = {}  #just for initaliation, later on gets filled with for tracking succes
+        self.rewards = {} #might be useful for tracking later on
+        self.max_energy = np.sum(np.square(self.model.actuator_ctrlrange[:,1]))
 
         # Define what the agent can observe
         # Dict space gives us structured, human-readable observations
@@ -27,14 +31,14 @@ class RobotWorldEnv(gym.MujocoEnv):
         target: 3 Target_pos xyz
         # """
 
-        number_of_agent_observations = self.robot_model.nq + 2*self.robot_model.nv
+        number_of_agent_observations = self.model.nq + 2*self.model.nv
         self.observation_space = gym.spaces.Dict(
             {
                 #Joint angles and join acc    
                 "agent": gym.spaces.Box(low=-np.inf, high=np.inf, shape=(number_of_agent_observations, ), dtype=np.float32), 
 
                 #sensor data, tcp acc, (later on maybe IMU)
-                "sensors": gym.spaces.Box(low=-np.inf, high=np.inf, shape=(self.robot_model.nsensor,), dtype = np.float32),
+                "sensors": gym.spaces.Box(low=-np.inf, high=np.inf, shape=(self.data.sensordata.shape[0],), dtype = np.float32),
 
                 #has to be determined by inverse kinematics in reality
                 "tcp_pos": gym.spaces.Box(low=-np.inf, high=np.inf, shape=(3,), dtype = np.float32),
@@ -45,8 +49,8 @@ class RobotWorldEnv(gym.MujocoEnv):
         )
 
         # Define what actions are available 
-        number_of_actuaters = self.robot_model.nu
-        self.action_space = gym.spaces.Box(low=-1, high=1, shape=(number_of_actuaters,), dtype=np.float32),   # motor drehmoment
+        number_of_actuaters = self.model.nu
+        self.action_space = gym.spaces.Box(low=-1, high=1, shape=(number_of_actuaters,), dtype=np.float32)   # motor drehmoment
 
 
         
@@ -61,13 +65,13 @@ class RobotWorldEnv(gym.MujocoEnv):
                                     self.data.qvel.flat[:], 
                                     self.data.qacc.flat[:]]).astype(np.float32)
         
-        sensor_obs = np.concatenate([self.data.sensor("gyro").data.flat[:], 
+        sensors_obs = np.concatenate([self.data.sensor("gyro").data.flat[:], 
                                     self.data.sensor("accel").data.flat[:]]).astype(np.float32)
 
         tcp_pos_obs = self.data.site("tcp").xpos.copy()
 
         return {"agent": agent_obs,
-                "sensor": sensor_obs,
+                "sensors": sensors_obs,
                 "tcp_pos": tcp_pos_obs,
                 "target": self.target_pos}
     
@@ -77,9 +81,7 @@ class RobotWorldEnv(gym.MujocoEnv):
         Returns:
             dict: Info about KPIs like distance, energy...
         """
-        return {
-            self.info
-        }
+        return self.info
     
     def reset(self, seed: Optional[int] = None, options: Optional[dict] = None):
         """Start a new episode.
@@ -94,27 +96,25 @@ class RobotWorldEnv(gym.MujocoEnv):
         # IMPORTANT: Must call this first to seed the random number generator
         super().reset(seed=seed)
 
+        self.steps_passed = 0
+
         # start the robot in a random configuration(radnom pos and speed)
-        rng = np.random.default_rng()
         #get joint_limits
-        joint_range_limits = self.robot_model.jnt_range #[[low1,high1][low2,high2]]
-        random_pos = rng.uniform(joint_range_limits[:,0], joint_range_limits[:,1], size=self.robot_model.nv)
+        joint_range_limits = self.model.jnt_range #[[low1,high1][low2,high2]]
+        random_pos = self.np_random.uniform(joint_range_limits[:,0], joint_range_limits[:,1], size=self.model.nv)
         
-        random_vel = rng.integers(low = 0.1, high= 1, size= self.robot_model.nv)
+        random_vel = self.np_random.uniform(low = -1, high= 1, size= self.model.nv)
         #write new positions to data with [:]
         self.data.qpos[:] = random_pos
         self.data.qvel[:] = random_vel
         mujoco.mj_forward(self.model, self.data)
 
         # Randomly place target, ensuring it's different from tcp pos
-        geoms = self.data.geom_size
-        lenghts = [np.max(geom) for geom in geoms]
-        max_range = sum(lenghts)
-        self.target_pos = rng.uniform(low=0.1, high= 0.8*max_range, size= 3)
+        self.target_pos = self.calculate_target_for_sphere()
         #if tcp pos and target are equal change target
         # FIXME: dont compare pos, change to distance greater than
         while np.array_equal(self.target_pos, self.data.site("tcp").xpos):
-            self.target_pos = rng.uniform(low=0.1, high= 0.8*max_range, size= 3)
+            self.target_pos = self.calculate_target_for_sphere()
 
         observation = self._get_obs()
         info = self._get_info()
@@ -130,31 +130,36 @@ class RobotWorldEnv(gym.MujocoEnv):
         Returns:
             tuple: (observation, reward, terminated, truncated, info)
         """
-        # Map the action to motor torque
+        self.steps_passed = self.steps_passed+1
 
-        direction = self._action_to_direction[action]
+        # Map the action to motor torque
+        torque = action*self.model.actuator_ctrlrange[:,1]
 
         # Update agent position, ensuring it stays within grid bounds
         # np.clip prevents the agent from walking off the edge
-        self._agent_location = np.clip(
-            self._agent_location + direction, 0, self.size - 1
-        )
+        self.data.ctrl[:] = torque
+        mujoco.mj_step(self.model, self.data)
+
+        tcp_pos = self.data.site("tcp").xpos
+        distance = np.linalg.norm(tcp_pos - self.target_pos)
 
         # Check if agent reached the target
-        terminated = np.array_equal(self._agent_location, self._target_location)
+        terminated = distance < self.goal_distance
 
         # We don't use truncation in this simple environment
         # (could add a step limit here if desired)
         truncated = False
 
+        #doesnt accuratly calculate energy, but strongly correlates
         
-        distance = np.linalg.norm(self._agent_location - self._target_location)
-        energy = ...
-        time = ...
+        energy = np.sum(np.square(action))
 
-        self.info = {"distance": distance,
-                     "energy": energy,
-                     "time": time
+        self.info = {
+                    "tcp" : tcp_pos,
+                    "target": self.target_pos,
+                    "distance": distance,
+                    "energy": energy,
+                    "steps_passed": self.steps_passed
                     }
         
 
@@ -165,5 +170,48 @@ class RobotWorldEnv(gym.MujocoEnv):
         return observation, reward, terminated, truncated, info
     
     def calculate_reward(self, measurements: dict):
-            reward = measurements
+            self.rewards = {}
+            
+            #distance worth the most [0, 10]
+            self.rewards["distance_reward"] = -3*measurements["distance"]
+            
+            #energy [-5, 0] 
+            self.rewards["energy_reward"] = -0.1*(measurements["energy"]/self.max_energy)
+
+            if measurements["distance"] < self.goal_distance:
+                self.rewards["goal_reward"] = 50
+            else:
+                self.rewards["goal_reward"] = 0
+
+            reward = np.sum(self.rewards.values())
             return reward
+    
+    def calculate_target_for_sphere(self):
+        geoms = self.data.geom_size
+        lenghts = [np.max(geom) for geom in geoms]
+        max_range = sum(lenghts)
+        phi = self.np_random.uniform(low=0, high= 2*np.pi, size=1)
+        theta = self.np_random.uniform(low=0, high= np.pi/2, size=1)
+        radius = self.np_random.uniform(low=0.1, high= 0.95*max_range, size=1)
+        x = radius*np.sin(theta)*np.cos(phi)
+        y = radius*np.sin(theta)*np.sin(phi)
+        z = radius*np.cos(theta)
+        return np.array([x,y,z].flatten())
+    
+    def get_rewards(self):
+        return self.rewards
+    
+    def render(self):
+        """Full chatty, keine ahnung was hier passiert"""
+        # Minimaler Render-Code für gym.Env
+        if self.render_mode == "human":
+            if self.viewer is None:
+                from mujoco import viewer
+                # Startet den passiven Viewer
+                self.viewer = viewer.launch_passive(self.model, self.data)
+            
+            self.viewer.sync()
+            
+            # Optional: Target visualisieren (wenn du eine Site 'target' im XML hast)
+            # site_id = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_SITE, "target")
+            # if site_id != -1: self.model.site_pos[site_id] = self.target_pos
