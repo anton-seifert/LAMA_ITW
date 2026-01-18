f5d68871c68645f4c815426ad616fe7a7f29e08a
Arne <urnye@student.kit.edu>
2026-01-18 22:36:48 +0100

    dummy version

--- train/test_train.py
+++ train/test_train.py
@@ -2,7 +2,7 @@ import gymnasium as gym
 import mujoco
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_checker import check_env
-from stable_baselines3.common.vec_env import VecNormalize, SubprocVecEnv, VecMonitor
+from stable_baselines3.common.vec_env import VecNormalize, SubprocVecEnv, VecMonitor, DummyVecEnv
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.callbacks import EvalCallback
 from typing import Optional
@@ -34,12 +34,12 @@ def train(env, config: Optional[dict] = None):
     check_env(env_control)
     env_control.close()
 
-    vec_check_env = make_vec_env(env, n_envs=1,env_kwargs={"model_path":modelpath}, vec_env_cls=SubprocVecEnv)
+    vec_check_env = make_vec_env(env, n_envs=1,env_kwargs={"model_path":modelpath}, vec_env_cls=DummyVecEnv)
     vec_check_env = VecMonitor(vec_check_env, filename=f"./monitor_logs/logs_check{timestamp}" )
     vec_check_env = VecNormalize(vec_check_env, norm_obs=True, norm_reward=False, training=False)
 
-    vec_train_env = make_vec_env(env,n_envs=8, env_kwargs={"model_path":modelpath},vec_env_cls=SubprocVecEnv)
-    vec_train_env = VecMonitor(vec_train_env, filename=f"./monitor_logs/logs_train{timestamp}" )
+    vec_train_env = make_vec_env(env,n_envs=1, env_kwargs={"model_path":modelpath},vec_env_cls=DummyVecEnv)
+    vec_train_env = VecMonitor(vec_train_env, filename=f"./monitor_logs/logs_train{timestamp}")    # is_sucessfull, usw. noch hinzufügen  , info_keywords=("distance") 
     
     vec_train_env = VecNormalize(vec_train_env , norm_obs=True, norm_reward=True)
    
@@ -49,7 +49,7 @@ def train(env, config: Optional[dict] = None):
 
     model = PPO("MultiInputPolicy" ,vec_train_env , device="cpu" ,tensorboard_log ="./tensorboard/ppo_test_robot_tensorboard/")
 
-    model.learn(total_timesteps=1_0_000,callback=eval_callback,progress_bar=True)
+    model.learn(total_timesteps=1_00_000,callback=eval_callback,progress_bar=True)
 
     
     model_save_path = f"{save_dir}/ppo_test_robot_{timestamp}"
