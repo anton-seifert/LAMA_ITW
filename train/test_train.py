4fc192f4fb8dbfe3aebbf0a47a9b83ae5c973ae6
Arne <urnye@student.kit.edu>
2026-01-18 22:12:22 +0100

    monitor + eval_callback

--- train/test_train.py
+++ train/test_train.py
@@ -2,8 +2,9 @@ import gymnasium as gym
 import mujoco
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_checker import check_env
-from stable_baselines3.common.vec_env import VecNormalize, SubprocVecEnv
+from stable_baselines3.common.vec_env import VecNormalize, SubprocVecEnv, VecMonitor
 from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.callbacks import EvalCallback
 from typing import Optional
 import os
 import sys
@@ -23,6 +24,7 @@ def train(env, config: Optional[dict] = None):
     folder_name = "assets"
     file_name = "test_robot.xml"
     modelpath = os.path.join(folder_name, file_name)
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
 
     save_dir = "models/ppo_training/"
     os.makedirs(save_dir, exist_ok=True)
@@ -32,28 +34,33 @@ def train(env, config: Optional[dict] = None):
     check_env(env_control)
     env_control.close()
 
-    vec_env = make_vec_env(env,n_envs=6, env_kwargs={"model_path":modelpath},vec_env_cls=SubprocVecEnv)
-    vec_env = VecNormalize(vec_env, norm_obs=True, norm_reward=True)
+    vec_check_env = make_vec_env(env, n_envs=1,env_kwargs={"model_path":modelpath}, vec_env_cls=SubprocVecEnv)
+    vec_check_env = VecMonitor(vec_check_env, filename=f"./monitor_logs/logs_check{timestamp}" )
+    vec_check_env = VecNormalize(vec_check_env, norm_obs=True, norm_reward=False, training=False)
 
-    model = PPO("MultiInputPolicy" ,vec_env, device="cpu" ,tensorboard_log ="./tensorboard/ppo_test_robot_tensorboard/", 
-                policy_kwargs = dict(net_arch=[256, 256]),
-                n_steps=4096,
-                batch_size=256,
-                ent_coef=0.001,
-                learning_rate=lambda progress: progress * 0.0003)
+    vec_train_env = make_vec_env(env,n_envs=8, env_kwargs={"model_path":modelpath},vec_env_cls=SubprocVecEnv)
+    vec_train_env = VecMonitor(vec_train_env, filename=f"./monitor_logs/logs_train{timestamp}" )
+    
+    vec_train_env = VecNormalize(vec_train_env , norm_obs=True, norm_reward=True)
+   
 
-    model.learn(total_timesteps=1_00_000,progress_bar=True)
+    eval_callback = EvalCallback(vec_check_env, best_model_save_path=f".models/ppo_training/best_models/best_model_{timestamp}", eval_freq=5000,deterministic=True, render=False)
 
-    timestamp = time.strftime("%Y%m%d-%H%M%S")
+
+    model = PPO("MultiInputPolicy" ,vec_train_env , device="cpu" ,tensorboard_log ="./tensorboard/ppo_test_robot_tensorboard/")
+
+    model.learn(total_timesteps=1_0_000,callback=eval_callback,progress_bar=True)
+
+    
     model_save_path = f"{save_dir}/ppo_test_robot_{timestamp}"
     stats_save_path = f"{save_dir}/ppo_test_robot_{timestamp}_vecnorm.pkl"
 
     model.save(model_save_path)
-    vec_env.save(stats_save_path)
+    vec_train_env.save(stats_save_path)
 
-    vec_env.close()
+    vec_train_env.close()
 
 if __name__ == '__main__':
 
     
-    train(RobotWorldEnv)
+   train(RobotWorldEnv)
