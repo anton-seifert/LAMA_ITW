6e87a019d54410719440f66b1fa0395f5a6da0c2
Anton Seifert <anton.seifert@web.de>
2026-01-19 12:36:16 +0100

    added basic parameters

--- configurations/config_test.py
+++ configurations/config_test.py
@@ -1,19 +1,35 @@
-print("hello, wir sind in der config")
+from envs.test_env import RobotWorldEnv
+
+"""
+file to define als the parameters for a training run
+if value is set to None, the standard value will be choosen
+"""
 
 Settings = {
+
     #GENERAL INFO
-    ""
-    "robot_model": "name of xml",
+    
+    "robot_model_path": "assets/test_robot.xml",
     "trained_model_path": "lalal",
     "trained_model_name": "lalal",
+    "render_mode": "human",
+    "seed" : 42,
+    "device" : "cpu",
 
     #ENV INFO
-    "distance reward" : 3,
-    "energy reward" : 0.2,
-    "goal reward" : 50,
+    "goal_distance" : 0.1,
+    "max steps": 1_000,
+
+    "distance_reward" : 20,
+    "energy_reward" : 0.2,
+    "goal_reward" : 50,
+    
 
     #TRAIN INFO
-    "n_step" : 10_000,
+    "env" : RobotWorldEnv,
+    "total_timesteps" : 100_000, #timesteps of WHOLE training
+    "n_step" : 10_000, #after n_steps the policy is updated
+    "eval_freq" : 5000,
 
     #SCORE
     "score": None
