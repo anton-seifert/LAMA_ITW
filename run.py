dc1142d4bc23dd36a8540c282d83013f98c313ee
Anton Seifert <anton.seifert@web.de>
2026-01-19 12:35:44 +0100

    succesfully load from config

--- run.py
+++ run.py
@@ -2,9 +2,6 @@ from typing import Optional
 
 #import subscripts
 
-from envs.test_env import RobotWorldEnv
-
-from train import test_train
 from train import render
 
 def main(config: Optional[dict] = None):
@@ -25,29 +22,28 @@ def main(config: Optional[dict] = None):
 
     #LOADING CONFIG
     if config != None:
-        print("loading config")
+        print("loading from config")
         #TODO: read from file
-        env_config = ...
-        train_config = ...
+        
     elif config == None:
+        print("NOT loading from config, defining a new one")
         env_config = {"distance_reward" : 0.1}
         train_config = {"Policy" : "MlpPolicy"}
 
     #TRAINING
-    #TODO: env creation auch ins training
-    env = make_vec_env(RobotWorldEnv(config),n_envs=6, env_kwargs={"model_path":modelpath},vec_env_cls=SubprocVecEnv)
-    test_train(env = env, config = config)
+    
+    train(config = config)
     #TODO: hier test run zu evaluation
 
     #RENDER Model
-    render(config)
+    #render(config)
 
 
 
-    
 
 if __name__ == "__main__" :
     from configurations.config_test import Settings as config_dict
+    from train.test_train import train
     main(config= config_dict)
 
     
