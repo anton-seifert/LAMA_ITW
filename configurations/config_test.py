c5557aa7fcbf2ec2cd69fc0f4ec4db4c058dc29b
Anton Seifert <anton.seifert@web.de>
2026-01-20 12:52:18 +0100

    added new paramas like kwars for model

--- configurations/config_test.py
+++ configurations/config_test.py
@@ -5,7 +5,7 @@ import time
 file to define als the parameters for a training run
 if value is set to None, the standard value will be choosen
 """
-
+#TODO: bei None werden die standard werte gewählt
 Settings = {
 
     #GENERAL INFO
@@ -25,15 +25,31 @@ Settings = {
     
 
     #TRAIN INFO
-    "env" : RobotWorldEnv, #sowas sollte eig als string eingeben werden und dann in den anderen scripten gemapped
-    "algo" : "PPO",
-    "total_timesteps" : 10_000_000, #timesteps of WHOLE training
+    "env" : "RobotWorldEnv",    #Options: RobotWorldEnv
+    "algo" : "PPO",             #Options: PPO, DDPG, SAC, TD3
+    "total_timesteps" : 120_000, #timesteps of WHOLE training
 
-    #TODO: currently not included
-    "n_step" : 10_000, #after n_steps the policy is updated
+    "model_kwargs": {
+    "learning_rate": 3e-4,     # Standard(3e-4)often is good enough
+    "n_steps": 2048,           # after n_steps the policy is updated
+    #"batch_size": 64,          
+    #"n_epochs": 10,
+    #"gamma": 0.99,
+    #"gae_lambda": 0.95,
+    #"clip_range": 0.2,
+    #"ent_coef": 0.0,           # Erhöhen auf 0.001, falls er zu schnell konvergiert
+    
+    # Architektur anpassen
+    "policy_kwargs": {
+        "net_arch": [256, 256], # Breiteres Netz lernt komplexe Physik besser
+        #"activation_fn": "torch.nn.Tanh" # Tanh ist oft besser für Continuous Control als ReLU
+            }
+    },
+
+    
     "eval_freq" : 5000,
-    "n_check_envs": ...,
-    "n_train_envs" : ...,
+    "n_check_envs": 16,
+    "n_train_envs" : 8,
 
     #SCORE
     "score": None,
