8c1f676286148876a857f62dc52b85035f3915e9
Anton Seifert <anton.seifert@web.de>
2026-01-20 12:53:58 +0100

    added mapping algo and env from string in config file

--- train/test_train.py
+++ train/test_train.py
@@ -1,11 +1,14 @@
 import gymnasium as gym
 import mujoco
-from stable_baselines3 import PPO
+from stable_baselines3 import PPO, SAC, TD3, DDPG
+from envs.test_env import RobotWorldEnv
 from stable_baselines3.common.env_checker import check_env
 from stable_baselines3.common.vec_env import VecNormalize, SubprocVecEnv, VecMonitor, DummyVecEnv
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.callbacks import EvalCallback
-from typing import Optional
+from stable_baselines3.common.base_class import BaseAlgorithm
+from typing import Optional, Type
+
 import os
 import sys
 import time
@@ -17,49 +20,68 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
 
-from envs.test_env import RobotWorldEnv
+
+
+ALGO_MAP = {
+    "PPO": PPO,
+    "SAC": SAC,
+    "TD3": TD3,
+    "DDPG": DDPG
+}
+
+ENV_MAP = {
+    "RobotWorldEnv" : RobotWorldEnv
+}
 
 def train(config: Optional[dict] = None):
 
-    #TODO: variablem mit config.get() holen
+    #variablen mit config.get() holen
     # first value is key in settings dict, secodn is fallback default value
-    modelpath = config.get("robot_model_path", "assets/test_robot.xml")
-    env = config.get("env", RobotWorldEnv)
+    env = config.get("env", "RobotWorldEnv")
     env_kwargs = ... #brauch man glaub net
     vec_env_cls = ... #was bist du?
     device = config.get("device", "cpu")
     verbose = ...#was bist du?
     total_timesteps = config.get("total_timesteps", 123_456)
-    algorithm = ...
     seed = config.get("seed", 42)
     eval_freq = config.get("eval_freq", 5555)
     timestamp = config.get("timestamp", 000000000)
     algo = config.get("algo")
+    n_check_envs = config.get("n_check_envs", 8)
+    n_train_envs = config.get("n_train_envs", 8)
+    hyperparams = config.get("model_kwargs")
+
+
+    #Mapping Algorithm from String
+    Algo_Class : Type[BaseAlgorithm] = ALGO_MAP[algo]
+    Env_Class = ENV_MAP[env]
 
     #modelpath = os.path.join(folder_name, file_name)
-    timestamp = time.strftime("%Y%m%d-%H%M%S")
+    #timestamp = time.strftime("%Y%m%d-%H%M%S")
 
     #save_dir = "models/ppo_training/"
     #os.makedirs(save_dir, exist_ok=True)
-
-    env_control = RobotWorldEnv(config=config)
+    print("\ncreating CONTROL env:")
+    env_control = Env_Class(config=config)
     #Checks if Costum env corresponds GymAPI  
     check_env(env_control)
     env_control.close()
 
-    vec_check_env = make_vec_env(env, seed = seed, n_envs=8, env_kwargs={"config":config}, vec_env_cls=SubprocVecEnv,monitor_dir=f"./monitor_logs/{algo}_training/logs_check_{timestamp}")
-   # vec_check_env = VecMonitor(vec_check_env, filename=f"./monitor_logs/logs_check{timestamp}" )
+    print(f"\ncreating {n_check_envs} CHECK envs:")
+    vec_check_env = make_vec_env(Env_Class, seed = seed, n_envs = n_check_envs, env_kwargs={"config":config}, vec_env_cls=SubprocVecEnv,monitor_dir=f"./monitor_logs/{algo}_training/logs_check_{timestamp}")
+    #TODO: ist das richtig so, dass norm_reward false ist,weil im train env ist das nicht so
     vec_check_env = VecNormalize(vec_check_env, norm_obs=True, norm_reward=False, training=False)
 
-    vec_train_env = make_vec_env(env, seed = seed,n_envs=8, env_kwargs={"config":config},vec_env_cls=SubprocVecEnv, monitor_dir=f"./monitor_logs/{algo}_training/logs_train_{timestamp}")
+    print(f"\ncreating {n_train_envs} TRAIN envs:")
+    vec_train_env = make_vec_env(Env_Class, seed = seed,n_envs = n_train_envs, env_kwargs={"config":config},vec_env_cls=SubprocVecEnv, monitor_dir=f"./monitor_logs/{algo}_training/logs_train_{timestamp}")
    # vec_train_env = VecMonitor(vec_train_env, filename=f"./monitor_logs/logs_train{timestamp}")    # is_sucessfull, usw. noch hinzufügen  , info_keywords=("distance") 
     vec_train_env = VecNormalize(vec_train_env , norm_obs=True, norm_reward=True)
    
 
     eval_callback = EvalCallback(vec_check_env, best_model_save_path=f"models/{algo}_training/best_models/best_model_{timestamp}", eval_freq=eval_freq, deterministic=True, render=False)
 
-    #TODO: also stark algorithm with the config and change parameters of algorithm object
-    model = PPO("MultiInputPolicy" ,vec_train_env, device= device, tensorboard_log=f"./tensorboard/{algo}_training/tensorboard_{timestamp}", verbose=0)
+    #TODO: also start algorithm with the config and change parameters of algorithm object
+    model = Algo_Class("MultiInputPolicy" ,**hyperparams, env = vec_train_env, device= device, tensorboard_log=f"./tensorboard/{algo}_training/tensorboard_{timestamp}", verbose=0)
 
     model.learn(total_timesteps= total_timesteps, callback=eval_callback, progress_bar=True)
 
