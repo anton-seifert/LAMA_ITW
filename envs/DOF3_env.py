7bc2f1c46f31849c5819a427002b20422555f14f
Anton Seifert <anton.seifert@web.de>
2026-01-26 14:03:51 +0100

    started pipeline for 3DOF robot

---
+++ envs/DOF3_env.py
@@ -0,0 +1,315 @@
+import gymnasium as gym
+import numpy as np
+import mujoco
+from gymnasium.envs.mujoco import MujocoEnv
+from typing import Optional
+from mujoco import viewer
+
+
+
+class RobotWorldEnv(gym.Env):
+    
+    def __init__(self, config: Optional[dict] = None, render_mode: Optional[str] = None, ):
+        print("creating env...")
+        #LOAD Variables from config
+        #first value = key from dict, second value fallback default value
+        self.model_path = config.get("robot_model_path")
+        self.goal_distance = config.get("goal_distance", 0.1)
+        self.max_steps = config.get("max_steps", 1_000)
+        self.distance_reward_factor = config.get("distance_reward", 20)
+        self.energy_reward_factor = config.get("energy_reward", 0.2)
+        self.goal_reward_factor = config.get("goal_reward", 50)
+        self.truncated_distance_steps = config.get("truncated_distance_steps")
+        self.truncated_distance_reward_factor = config.get("trunacated_distance_reward")
+        
+
+        #load model from Path
+        self.model = mujoco.MjModel.from_xml_path(self.model_path)
+        self.data = mujoco.MjData(self.model)
+
+        self.target_pos = np.array([1,1,1]) # just for initialition
+        self.steps_passed = 0
+        self.info = {}  #just for initaliation, later on gets filled with for tracking succes
+        self.rewards = {} #might be useful for tracking later on
+        self.max_energy = np.sum(np.square(self.model.actuator_ctrlrange[:,1]))
+
+        self.viewer = None
+        self.render_mode = render_mode
+
+        # Define what the agent can observe
+        # Dict space gives us structured, human-readable observations
+
+        # TCP Pos, TCP Acceleration, Joint Angles, Joint Velocity, Joint Acceleration, Target Pos
+
+        """
+        nv = degrees of freedom, also eig die number of velocities, or acceleration
+        nq = number of generalized coords, die angle position (das kracht beim continums robot, weil dann quateriums oder so benutzt werden, nicht 3 komponenten, sondern 4)
+        sensor 3 TCP Acc, 3 TCP Gyro
+        kinematic: 3 TCP pos, xyz
+        target: 3 Target_pos xyz
+        # """
+
+        number_of_agent_observations = self.model.nq + 2*self.model.nv
+        self.observation_space = gym.spaces.Dict(
+            {
+                #Joint angles, joint vel, join acc   
+                "agent": gym.spaces.Box(low=-np.inf, high=np.inf, shape=(number_of_agent_observations, ), dtype=np.float32), 
+
+                #sensor data, tcp acc, (later on maybe IMU)
+                "sensors": gym.spaces.Box(low=-np.inf, high=np.inf, shape=(self.data.sensordata.shape[0],), dtype = np.float32),
+
+                #has to be determined by inverse kinematics in reality
+                "tcp_pos": gym.spaces.Box(low=-np.inf, high=np.inf, shape=(3,), dtype = np.float32),
+
+                #Target Pos
+                "target": gym.spaces.Box(low=-np.inf,high=np.inf, shape=(3,), dtype=np.float64),  # [x, y, z] coordinates
+            }
+        )
+
+        # Define what actions are available 
+        number_of_actuaters = self.model.nu
+        self.action_space = gym.spaces.Box(low=-1, high=1, shape=(number_of_actuaters,), dtype=np.float32)   # motor drehmoment
+
+
+        
+    def _get_obs(self):
+        """Convert internal state to observation format.
+
+        Returns:
+            dict: Observation with agent and target positions
+        """
+        
+        agent_obs = np.concatenate([self.data.qpos.flat[:], 
+                                    self.data.qvel.flat[:], 
+                                    self.data.qacc.flat[:]]).astype(np.float32)
+        
+        sensors_obs = np.concatenate([self.data.sensor("gyro").data.flat[:], 
+                                    self.data.sensor("accel").data.flat[:]]).astype(np.float32)
+
+        tcp_pos_obs = self.data.site("tcp").xpos.copy().astype(np.float32)
+
+        return {"agent": agent_obs,
+                "sensors": sensors_obs,
+                "tcp_pos": tcp_pos_obs,
+                "target": self.target_pos}
+    
+    def _get_info(self):
+        """Compute auxiliary information for debugging.
+
+        Returns:
+            dict: Info about KPIs like distance, energy...
+        """
+        return self.info
+    
+    def reset(self, seed: Optional[int] = None, options: Optional[dict] = None):
+        """Start a new episode.
+
+        Args:
+            seed: Random seed for reproducible episodes
+            options: Additional configuration (unused in this example)
+
+        Returns:
+            tuple: (observation, info) for the initial state
+        """
+        # IMPORTANT: Must call this first to seed the random number generator
+        super().reset(seed=seed)
+
+        self.steps_passed = 0
+
+        # start the robot in a random configuration(radnom pos and speed)
+        #get joint_limits
+        joint_range_limits = self.model.jnt_range #[[low1,high1][low2,high2]]
+        random_pos = self.np_random.uniform(joint_range_limits[:,0], joint_range_limits[:,1], size=self.model.nv)
+        
+        random_vel = self.np_random.uniform(low = -1, high= 1, size= self.model.nv)
+        #write new positions to data with [:]
+        self.data.qpos[:] = random_pos
+        self.data.qvel[:] = random_vel
+        mujoco.mj_forward(self.model, self.data)
+
+
+        # Randomly place target, ensuring it's different from tcp pos
+        self.target_pos = self.calculate_target_for_sphere()
+        tcp_pos = self.data.site("tcp").xpos
+        distance = np.linalg.norm(tcp_pos - self.target_pos)
+        #if tcp pos and target are too close, look for new target
+        while (distance <= 5*self.goal_distance):
+            #print("calc new target")
+            self.target_pos = self.calculate_target_for_sphere()
+            distance = np.linalg.norm(tcp_pos - self.target_pos)
+
+        self.info["best_distance"] = distance
+        self.info["best_distance_step"] = 0
+        observation = self._get_obs()
+        info = self._get_info()
+
+        return observation, info
+    
+    def step(self, action):
+        """Execute one timestep within the environment.
+
+        Args:
+            action: The action to take 
+
+        Returns:
+            tuple: (observation, reward, terminated, truncated, info)
+        """
+        self.steps_passed = self.steps_passed+1
+        terminated = False
+        # Map the action to motor torque
+        torque = action*self.model.actuator_ctrlrange[:,1]
+
+        # Update agent position, ensuring it stays within grid bounds
+        # np.clip prevents the agent from walking off the edge
+        self.data.ctrl[:] = torque
+        mujoco.mj_step(self.model, self.data)
+
+        tcp_pos = self.data.site("tcp").xpos
+        distance = np.linalg.norm(tcp_pos - self.target_pos)
+
+        reached_target = False
+        
+        # Check if agent reached the target
+        if (distance < self.goal_distance):
+            terminated = True
+            reached_target = True
+
+        # Truncated after set step limit
+        truncated = self.steps_passed > self.max_steps
+
+        #Updates best(smallest) distance
+        if(distance < self.info["best_distance"]):
+            self.info["best_distance"] = distance
+            self.info["best_distance_step"] = self.steps_passed
+
+        #terminates if distance didnt get smaller after set time steps
+        truncated_distance = False
+        if(self.steps_passed- self.info["best_distance_step"] > self.truncated_distance_steps):
+            truncated_distance = True
+            truncated = True
+
+
+        #doesnt accuratly calculate energy, but strongly correlates
+        energy = np.sum(np.square(action))
+
+        self.info.update({
+                    "tcp" : tcp_pos,
+                    "target": self.target_pos,
+                    "distance": distance,
+                    "energy": energy,
+                    "steps_passed": self.steps_passed,
+                    "reached_target": reached_target,
+                    "truncated_distance": truncated_distance
+                    })
+        
+        #render if render_mode is specified, skip if none
+        if self.render_mode == "human":
+            self.render()
+        elif self.render_mode == "rgb_array":
+            self.render()
+
+        reward = self.calculate_reward(self.info)
+        observation = self._get_obs()
+        info = self.info
+
+        return observation, reward, terminated, truncated, info
+    
+    def calculate_reward(self, measurements: dict):
+            self.rewards = {}
+            
+            #distance worth the most 
+            self.rewards["distance_reward"] = -self.distance_reward_factor*measurements["distance"]
+            
+            #energy  
+            self.rewards["energy_reward"] = -self.energy_reward_factor*(measurements["energy"]/self.max_energy)
+
+            #punish if gets truncated beacuse of no distance improvemnts after set steps
+            if(self.info["truncated_distance"]):
+                self.rewards["truncated_distance"] = -self.truncated_distance_reward_factor
+
+            if measurements["distance"] < self.goal_distance:
+                self.rewards["goal_reward"] = self.goal_reward_factor
+            else:
+                self.rewards["goal_reward"] = 0
+
+            reward = sum(self.rewards.values())
+            return reward
+    
+    def calculate_target_for_sphere(self):
+        geoms = self.model.geom_size
+        lenghts = [np.max(geom) for geom in geoms]
+        max_range = sum(lenghts)
+        phi = self.np_random.uniform(low=0, high= 2*np.pi)
+        theta= 0
+        if(self.model_path == "assets/test_robot.xml"):
+            theta = np.pi/2 
+        elif(self.model == "assets/test_robot_3DOF"):
+            theta = self.np_random.uniform(low=0, high= np.pi/2)
+        #HACK: theta set to 90° for planar z = 0, max radius is set to fixed value
+        
+        radius = self.np_random.uniform(low=0.1, high= 0.55)
+        x = radius*np.sin(theta)*np.cos(phi)
+        y = radius*np.sin(theta)*np.sin(phi)
+        z = radius*np.cos(theta)
+        return np.array([x,y,z]).flatten()
+    
+    def get_rewards(self):
+        return self.rewards
+    
+    def render(self):
+        """Full chatty, keine ahnung was hier passiert"""
+        #------
+        # diesen code block nach unten verschieben, wenn man das target immer in der gleichen stelle haben will
+
+        # visalize target with movable body named target
+        body_id = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_BODY, "target")
+        if body_id != -1:
+            # Die Mocap-ID holen (Mapping von Body -> Mocap)
+            mocap_id = self.model.body_mocapid[body_id]
+
+        if mocap_id != -1:
+            self.data.mocap_pos[mocap_id] = self.target_pos 
+            # update kinematics
+            mujoco.mj_forward(self.model, self.data) 
+        #---------------
+
+        # Minimaler Render-Code für gym.Env
+        if self.render_mode == "rgb_array":
+            return self._get_rgb_array()
+
+        if self.render_mode == "human":
+            if self.viewer is None:
+                # Startet den passiven Viewer
+                self.viewer = viewer.launch_passive(self.model, self.data)
+            
+            self.viewer.sync()
+            
+        
+
+        
+
+    def _get_rgb_array(self):
+        """ Full Chatty, keine Ahnung was hier geht"""
+    # Renderer wird nur beim ersten Aufruf erstellt (Lazy Loading)
+        if not hasattr(self, 'renderer'):
+        # WICHTIG: width/height bestimmen die Video-Auflösung
+            self.renderer = mujoco.Renderer(self.model, height=480, width=640)
+    
+    # Die aktuelle Physik-Szene in den Renderer laden
+        self.renderer.update_scene(self.data) 
+    # Falls du eine bestimmte Kamera hast: update_scene(self.data, camera="kamera_name")
+    
+    # Das Bild als Numpy-Array zurückgeben
+        return self.renderer.render()
+    
+
+    """fully chatty"""
+    def close(self):
+    # Renderer löschen, falls er existiert
+        if hasattr(self, 'renderer'):
+            del self.renderer
+    
+    # Viewer schließen (hast du wahrscheinlich schon)
+        if self.viewer is not None:
+            self.viewer.close()
+        super().close()
---
+++ train/DOF3_train.py
@@ -0,0 +1,169 @@
+from stable_baselines3 import PPO, SAC, TD3, DDPG
+from stable_baselines3.common.env_checker import check_env
+from stable_baselines3.common.vec_env import VecNormalize, SubprocVecEnv, VecMonitor, DummyVecEnv
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.callbacks import EvalCallback, StopTrainingOnNoModelImprovement
+from stable_baselines3.common.base_class import BaseAlgorithm
+from typing import Optional, Type
+import wandb
+from wandb.integration.sb3 import WandbCallback
+from stable_baselines3.common.vec_env import VecVideoRecorder, DummyVecEnv
+from utils import AutoSaveVecNormalize
+
+
+import os
+import sys
+import time
+
+
+
+#Add parent directory to sys.path to resolve cross-directory imports from sibling packages
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+from envs.test_env import RobotWorldEnv
+
+
+ALGO_MAP = {
+    "PPO": PPO,
+    "SAC": SAC,
+    "TD3": TD3,
+    "DDPG": DDPG
+}
+
+ENV_MAP = {
+    "RobotWorldEnv" : RobotWorldEnv
+}
+
+#Maps the number of train_envs to PC
+N_ENV_MAP = {
+    "An" : 11,  
+    "Ar": 15,
+    "IT" :11
+}
+
+
+def train(config: Optional[dict] = None):
+
+    #variablen mit config.get() holen
+    # first value is key in settings dict, secodn is fallback default value
+    env = config.get("env", "RobotWorldEnv")
+    device = config.get("device", "cpu")
+    verbose = ...#was bist du?
+    timesteps_per_env = config.get("timesteps_per_env", 100)
+    seed = config.get("seed", 42)
+    eval_freq = config.get("eval_freq", 5555)
+    timestamp = config.get("timestamp", 000000000)
+    algo = config.get("algo")
+    n_check_envs = config.get("n_check_envs", 5)
+    hyperparams = config.get("model_kwargs")
+    n_eval_episodes = config.get("n_eval_episodes", 20)
+    pc = config.get("PC", "An")
+
+    
+    #Mapping Variables from String
+    Algo_Class : Type[BaseAlgorithm] = ALGO_MAP[algo]
+    Env_Class = ENV_MAP[env]
+    n_train_envs = N_ENV_MAP[pc]
+    print(f"\nRunning {algo} in the {env} Environmenet on {pc} PC")
+
+    total_timesteps = timesteps_per_env*n_train_envs
+
+    #Init Weights and Biases
+    run = wandb.init(
+        project="Bachelor_Robot_RL", 
+        config=config, 
+        sync_tensorboard=True,
+        monitor_gym= False #true if you want to upload every video done in training 
+    )
+    wandb.save("configurations/config_3DOF.py", base_path="configurations", policy="now")
+
+    print("\ncreating CONTROL env:")
+    env_control = Env_Class(config=config)
+    #Checks if Costum env corresponds GymAPI  
+    check_env(env_control)
+    env_control.close()
+
+    print(f"\ncreating {n_check_envs} CHECK envs:")
+    vec_check_env = make_vec_env(
+        Env_Class,
+        seed = 1111,
+        n_envs = n_check_envs,
+        env_kwargs={"config":config},
+        vec_env_cls=DummyVecEnv,
+        monitor_dir=f"./monitor_logs/{algo}_training/logs_check_{timestamp}",
+        monitor_kwargs= {"info_keywords": ("distance","energy","reached_target","truncated_distance")}
+        )
+    #TODO: ist das richtig so, dass norm_reward false ist,weil im train env ist das nicht so
+    vec_check_env = VecNormalize(vec_check_env, norm_obs=True, norm_reward=False, training=False)
+
+    """vec_check_env = VecVideoRecorder(
+        vec_check_env,
+        video_folder=f"eval_videos/{timestamp}/{run_id}_eval",
+        record_video_trigger=lambda x: x == 0, # Nimmt ab dem ersten Step auf
+        video_length=1000,                     # Wie lang das Video max sein darf
+        name_prefix=f"eval-agent"
+    )"""
+
+    print(f"\ncreating {n_train_envs} TRAIN envs:")
+    
+    stop_train_callback = StopTrainingOnNoModelImprovement(max_no_improvement_evals=10 , min_evals=5)
+
+    vec_train_env = make_vec_env(
+        Env_Class,
+        seed = seed,
+        n_envs = n_train_envs,
+        env_kwargs={"config":config},
+        vec_env_cls=SubprocVecEnv,
+        monitor_dir=f"./monitor_logs/{algo}_training/logs_train_{timestamp}",
+        monitor_kwargs= {"info_keywords": ("distance","energy","reached_target","truncated_distance")}
+        )
+    #TODO: add additional infos
+    #vec_train_env = VecMonitor(vec_train_env, filename=f"./monitor_logs/logs_train{timestamp}")    # is_sucessfull, usw. noch hinzufügen  , info_keywords=("distance") 
+    vec_train_env = VecNormalize(vec_train_env , norm_obs=True, norm_reward=True)
+    
+    #initialize custon callback for saving vecnorm
+    vecnorm_save_path = f"models/{algo}_training/best_models/best_model_{timestamp}"
+    save_stats = AutoSaveVecNormalize(vecnorm_save_path)
+
+    eval_callback = EvalCallback(
+        vec_check_env,
+        best_model_save_path=f"models/{algo}_training/best_models/best_model_{timestamp}",
+        eval_freq=eval_freq,
+        n_eval_episodes = n_eval_episodes,
+        deterministic=True,
+        render=False,
+        callback_after_eval=stop_train_callback,
+        callback_on_new_best= save_stats
+        )
+    
+    wandb_callback = WandbCallback(
+        verbose=2,
+        model_save_path=None, # <-- Dont save to cloud during training only at the end
+        model_save_freq=0
+    )
+    #Creating Model
+    model = Algo_Class("MultiInputPolicy" ,**hyperparams, env = vec_train_env, device= device, tensorboard_log=f"./tensorboard/{algo}_training/tensorboard_{timestamp}", verbose=0)
+
+    model.learn(total_timesteps= total_timesteps, callback=[eval_callback, wandb_callback], progress_bar=True)
+
+    #model_save_path = f"models/{algo}_training/models/model_{timestamp}"
+
+    #stats_save_path = f"models/{algo}_training/models_vecnorm/vecnorm_{timestamp}_vecnorm.pkl"
+    
+    #Upload to WanB
+    #TODO: safe best vecnorm
+    wandb.save(f"models/{algo}_training/best_models/best_model_{timestamp}/best_model.zip", base_path=f"models/{algo}_training/best_models/best_model_{timestamp}")
+    wandb.save(f"models/{algo}_training/best_models/best_model_{timestamp}/vec_normalize.pkl", base_path=f"models/{algo}_training/best_models/best_model_{timestamp}")
+    
+    #model.save(model_save_path)
+    #vec_train_env.save(stats_save_path)
+
+    vec_train_env.close()
+    vec_check_env.close()
+
+if __name__ == '__main__':
+
+    
+   train(RobotWorldEnv)
