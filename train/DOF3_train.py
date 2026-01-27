c52fa72a0a004580e3e1850877173c54eb87383a
Anton Seifert <anton.seifert@web.de>
2026-01-27 15:46:51 +0100

    loading 3DOF env

--- train/DOF3_train.py
+++ train/DOF3_train.py
@@ -22,7 +22,7 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
 
-from envs.test_env import RobotWorldEnv
+from envs.DOF3_env import RobotWorldEnv
 
 
 ALGO_MAP = {
